90d8d821bfc06c951642eba4ca710b3534155de4
pabather <p.a.bather1@gmail.com>
2023-11-25 18:56:26 +0100

    added app entry point

---
+++ src/main/java/com/philippabather/properpropertiesapi/ProperPropertiesApiApplication.java
@@ -0,0 +1,13 @@
+package com.philippabather.properpropertiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProperPropertiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProperPropertiesApiApplication.class, args);
+	}
+
+}
