6d01add3e4e7ca8ca595ed7168dafcf2006bb8cb
pabather <p.a.bather1@gmail.com>
2023-11-26 20:01:38 +0100

    added error handling

--- src/main/java/com/philippabather/properpropertiesapi/exception/error/Response.java
+++ src/main/java/com/philippabather/properpropertiesapi/exception/error/Response.java
@@ -1,2 +1,22 @@
-package com.philippabather.properpropertiesapi.exception.error;public class Response {
+package com.philippabather.properpropertiesapi.exception.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Response - clase define la respuesta de un error
+ *
+ * @author Philippa Bather
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+    private String code;
+    private String type;
+    private String message;
 }
--- src/main/java/com/philippabather/properpropertiesapi/exception/error/ValidationErrorResponseModel.java
+++ src/main/java/com/philippabather/properpropertiesapi/exception/error/ValidationErrorResponseModel.java
@@ -1,2 +1,23 @@
-package com.philippabather.properpropertiesapi.exception.error;public class ValidationErrorResponseModel {
+package com.philippabather.properpropertiesapi.exception.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * ValidationErrorResponseModel - la clase define el modelo de respuesta de errores de validación
+ *
+ * @author Philippa Bather
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidationErrorResponseModel {
+    private String type;
+    private List<ValidationErrorModel> errors;
 }
