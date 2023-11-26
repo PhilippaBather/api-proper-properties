e662b14274dde00b72e1bb3d157e414d46c319a0
pabather <p.a.bather1@gmail.com>
2023-11-26 20:02:30 +0100

    updated classes and validation

--- src/main/java/com/philippabather/properpropertiesapi/model/Client.java
+++ src/main/java/com/philippabather/properpropertiesapi/model/Client.java
@@ -1,5 +1,6 @@
 package com.philippabather.properpropertiesapi.model;
 
+import com.philippabather.properpropertiesapi.constants.ValidationMessages;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -9,6 +10,8 @@ import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
+
+import static com.philippabather.properpropertiesapi.constants.ValidationMessages.VALIDATION_DOB_NOT_NULL;
 
 /**
  * Client - el usuario que busca un inmueble.
@@ -29,11 +32,12 @@ public class Client extends User {
     @Column(name = "id", updatable = false, nullable = false)
     private long id;
 
-    @NotNull(message = "Date of birth is required")
+    @NotNull(message = VALIDATION_DOB_NOT_NULL)
     @DateTimeFormat
     @Column
     private LocalDateTime dob; // fecha de nacimiento (date of birth)
 
+    @NotNull(message= ValidationMessages.VALIDATION_BOOLEAN_REQUIRED)
     @Column(name = "is_student")
-    private boolean isStudent;
+    private Boolean isStudent;
 }
