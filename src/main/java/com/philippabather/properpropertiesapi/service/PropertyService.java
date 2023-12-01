c0f5dfdf21133655e851ba099c57ccfa5258d323
pabather <p.a.bather1@gmail.com>
2023-12-01 14:44:44 +0100

    added property service interface and implementation

--- src/main/java/com/philippabather/properpropertiesapi/service/PropertyService.java
+++ src/main/java/com/philippabather/properpropertiesapi/service/PropertyService.java
@@ -1,2 +1,23 @@
-package com.philippabather.properpropertiesapi.service;public interface PropertyService {
+package com.philippabather.properpropertiesapi.service;
+
+import com.philippabather.properpropertiesapi.exception.PropertyNotFoundException;
+import com.philippabather.properpropertiesapi.model.Property;
+
+import java.util.List;
+import java.util.PropertyPermission;
+import java.util.Set;
+
+/**
+ * PropertyService - la interfaz para manejar la entidad Property ('inmueble').
+ *
+ * @author Philippa Bather
+ */
+public interface PropertyService {
+
+    // TODO - change list to SET
+    List<Property> getAll();
+    Property save (Property property);
+
+    Property getById(long propertyId) throws PropertyNotFoundException;
+    void deleteById(long propertyId) throws PropertyNotFoundException;
 }
--- src/main/java/com/philippabather/properpropertiesapi/service/PropertyServiceImpl.java
+++ src/main/java/com/philippabather/properpropertiesapi/service/PropertyServiceImpl.java
@@ -1,2 +1,45 @@
-package com.philippabather.properpropertiesapi.service;public class PropertyServiceImpl {
+package com.philippabather.properpropertiesapi.service;
+
+import com.philippabather.properpropertiesapi.exception.PropertyNotFoundException;
+import com.philippabather.properpropertiesapi.model.Property;
+import com.philippabather.properpropertiesapi.repository.PropertyRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+/**
+ * PropertyServiceImpl - implementa la interfaz PropertyService; maneja información sobre la entidad Property ('inmueble').
+ *
+ * @author Philippa Bather
+ */
+@Service
+public class PropertyServiceImpl implements PropertyService {
+
+    private final PropertyRepository propertyRepo;
+
+    public PropertyServiceImpl(PropertyRepository propertyRepo) {
+        this.propertyRepo = propertyRepo;
+    }
+
+    @Override
+    public List<Property> getAll() {
+        return propertyRepo.findAll();
+    }
+
+    @Override
+    public Property save(Property property) {
+        return propertyRepo.save(property);
+    }
+
+    @Override
+    public Property getById(long propertyId) throws PropertyNotFoundException {
+        return propertyRepo.findById(propertyId).orElseThrow(() -> new PropertyNotFoundException(propertyId));
+    }
+
+    @Override
+    public void deleteById(long propertyId) throws PropertyNotFoundException {
+        Property property = propertyRepo.findById(propertyId).orElseThrow(() -> new PropertyNotFoundException(propertyId));
+        propertyRepo.delete(property);
+    }
 }
