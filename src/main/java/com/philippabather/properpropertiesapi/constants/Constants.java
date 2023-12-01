a00092daa0ad049359caff18aeca4151c4e20eed
pabather <p.a.bather1@gmail.com>
2023-12-01 14:40:55 +0100

    updated constants

---
+++ src/main/java/com/philippabather/properpropertiesapi/constants/Constants.java
@@ -0,0 +1,14 @@
+package com.philippabather.properpropertiesapi.constants;
+
+/**
+ * Constants - constantes generales
+ *
+ * @author Philippa Bather
+ */
+public class Constants {
+
+    public static final int LATITUDE_MIN = -90;
+    public static final int LATITUDE_MAX = 90;
+    public static final int LONGITUDE_MIN = -180;
+    public static final int LONGITUDE_MAX = 180;
+}
--- src/main/java/com/philippabather/properpropertiesapi/constants/ValidationMessages.java
+++ src/main/java/com/philippabather/properpropertiesapi/constants/ValidationMessages.java
@@ -9,8 +9,13 @@ public class ValidationMessages {
 
     public static final String VALIDATION_ACCOUNT_CREATION_NOT_NULL = "Date of account creation is required.";
     public static final String VALIDATION_DOB_NOT_NULL = "Date of birth is required";
+    public static final String VALIDATION_DATE = "A valid date is required.";
     public static final String VALIDATION_EMAIL = "A valid email is required.";
+    public static final String VALIDATION_LATITUDE_REQUIRED = "A valid latitude is required.";
+    public static final String VALIDATION_LONGITUDE_REQUIRED = "A valid longitude is required.";
     public static final String VALIDATION_NAME_NOT_BLANK = "A name is required.";
+    public static final String VALIDATION_NUM_BEDROOMS = "The number of bedrooms is required.";
+    public static final String VALIDATION_NUM_BATHROOMS = "The number of bathrooms is required.";
     public static final String VALIDATION_PASSWORD = "Password must contain minimum 8 characters, including at least 1 digit and 1 special character; the maximum length is of 25 characters";
     public static final String VALIDATION_SURNAME_NOT_BLANK = "Surname is required.";
     public static final String VALIDATION_USERNAME_NOT_BLANK = "Username must not be blank.";
