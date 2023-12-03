862f12f81722daf382ad381a05ca5e594a38f098
pabather <p.a.bather1@gmail.com>
2023-12-03 11:10:59 +0100

    added proprietor dtos

--- src/main/java/com/philippabather/properpropertiesapi/dto/ProprietorDTOIn.java
+++ src/main/java/com/philippabather/properpropertiesapi/dto/ProprietorDTOIn.java
@@ -1,2 +1,30 @@
-package com.philippabather.properpropertiesapi.dto;public class ProprietorDTOIn {
+package com.philippabather.properpropertiesapi.dto;
+
+import com.philippabather.properpropertiesapi.constants.ValidationMessages;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+/**
+ * ClientDTO - Data Transfer Object (DTO) para la clase 'Client' que extiende la clase abstracta de User.
+ *
+ * @author Philippa Bather
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class ProprietorDTOIn extends UserDTOIn {
+
+    @Min(1)
+    private int numProperties;
+
+    @NotNull(message= ValidationMessages.VALIDATION_BOOLEAN_REQUIRED)
+    private boolean isAgency;
+
+
 }
--- src/main/java/com/philippabather/properpropertiesapi/dto/ProprietorDTOOut.java
+++ src/main/java/com/philippabather/properpropertiesapi/dto/ProprietorDTOOut.java
@@ -1,2 +1,23 @@
-package com.philippabather.properpropertiesapi.dto;public class ProprietorDTOOut {
+package com.philippabather.properpropertiesapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * ProprietorDTOOut - El objeto de la transferencia de datos (DTO) de la clase Proprietor al cliente/consumo de API; extiende UserDTOOut.
+ *
+ * @author Philippa Bather
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class ProprietorDTOOut extends UserDTOOut {
+
+    private long id;
+    private int numProperties;
+    private boolean isAgency;
+
 }
