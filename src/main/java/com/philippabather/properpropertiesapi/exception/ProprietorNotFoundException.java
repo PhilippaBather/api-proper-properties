776333b85128646fa56964863db03751c8fe33a6
pabather <p.a.bather1@gmail.com>
2023-12-03 11:11:44 +0100

    updated exceptions and validation to handle proprietor

--- src/main/java/com/philippabather/properpropertiesapi/exception/ProprietorNotFoundException.java
+++ src/main/java/com/philippabather/properpropertiesapi/exception/ProprietorNotFoundException.java
@@ -1,2 +1,10 @@
-package com.philippabather.properpropertiesapi.exception;public class ProprietorNotFoundException {
+package com.philippabather.properpropertiesapi.exception;
+
+import static com.philippabather.properpropertiesapi.constants.ErrorMessages.PROPRIETOR_NOT_FOUND_EXCEPTION_MSG;
+
+public class ProprietorNotFoundException extends RuntimeException {
+
+    public ProprietorNotFoundException(long proprietorId) {
+        super(PROPRIETOR_NOT_FOUND_EXCEPTION_MSG + proprietorId);
+    }
 }
