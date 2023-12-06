8e40874d6923ade4de4d1909bf1cecd6dc300851
pabather <p.a.bather1@gmail.com>
2023-12-06 19:52:22 +0100

    updated to include exceptions for property enums

--- src/main/java/com/philippabather/properpropertiesapi/exception/PropertyStatusNotFoundException.java
+++ src/main/java/com/philippabather/properpropertiesapi/exception/PropertyStatusNotFoundException.java
@@ -1,2 +1,8 @@
-package com.philippabather.properpropertiesapi.exception;public class PropertyStatusNotFoundException {
+package com.philippabather.properpropertiesapi.exception;
+
+public class PropertyStatusNotFoundException extends RuntimeException {
+
+    public PropertyStatusNotFoundException(String propertyStatus) {
+        super(String.format("Property status %s not found; valid statuses are RENTAL or SALE", propertyStatus));
+    }
 }
--- src/main/java/com/philippabather/properpropertiesapi/exception/PropertyTypeNotFoundException.java
+++ src/main/java/com/philippabather/properpropertiesapi/exception/PropertyTypeNotFoundException.java
@@ -1,2 +1,8 @@
-package com.philippabather.properpropertiesapi.exception;public class PropertyTypeNotFoundException {
+package com.philippabather.properpropertiesapi.exception;
+
+public class PropertyTypeNotFoundException extends RuntimeException {
+
+    public PropertyTypeNotFoundException(String propertyType) {
+        super(String.format("Property type %s not found; valid types are: COMMERCIAL, FLAT, or HOUSE", propertyType));
+    }
 }
