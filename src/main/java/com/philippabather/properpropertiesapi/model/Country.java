b5dc02bdc67937220aa252e9dfee849347821b8c
pabather <p.a.bather1@gmail.com>
2023-12-06 23:16:20 +0100

    added Address class and Region and Country enums

--- src/main/java/com/philippabather/properpropertiesapi/model/Country.java
+++ src/main/java/com/philippabather/properpropertiesapi/model/Country.java
@@ -1,2 +1,11 @@
-package com.philippabather.properpropertiesapi.model;public enum Country {
+package com.philippabather.properpropertiesapi.model;
+
+/**
+ * Country - un país
+ *
+ * @author Philippa Bather
+ */
+public enum Country {
+
+    SPAIN
 }
--- src/main/java/com/philippabather/properpropertiesapi/model/Region.java
+++ src/main/java/com/philippabather/properpropertiesapi/model/Region.java
@@ -1,2 +1,30 @@
-package com.philippabather.properpropertiesapi.model;public enum Region {
+package com.philippabather.properpropertiesapi.model;
+
+/**
+ * Region - una región de España
+ *
+ * @author Philippa Bather
+ */
+public enum Region {
+
+    ANDALUSIA,
+    ARAGON,
+    ASTURIAS,
+    BALEARIC_ISLANDS,
+    BASQUE_COUNTRY,
+    CANARY_ISLANDS,
+    CANTABRIA,
+    CASTILE_AND_LEON,
+    CASTILE_LA_MANCHA,
+    CATALONIA,
+    CEUTA,
+    EXTREMADURA,
+    GALICIA,
+    LA_RIOJA,
+    MADRID,
+    MURCIA,
+    MELILLA,
+    NAVARRA,
+    VALENCIA
+
 }
