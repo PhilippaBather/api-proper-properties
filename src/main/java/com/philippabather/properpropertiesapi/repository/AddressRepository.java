dae565dfd1329b0258a450d1262ff8ed110c664f
pabather <p.a.bather1@gmail.com>
2023-12-06 23:16:50 +0100

    added address repository

--- src/main/java/com/philippabather/properpropertiesapi/repository/AddressRepository.java
+++ src/main/java/com/philippabather/properpropertiesapi/repository/AddressRepository.java
@@ -1,2 +1,25 @@
-package com.philippabather.properpropertiesapi.repository;public interface AddressRepository {
+package com.philippabather.properpropertiesapi.repository;
+
+import com.philippabather.properpropertiesapi.model.Address;
+import com.philippabather.properpropertiesapi.model.Region;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * AddressRepository - el repositorio para manejar la entidad Address ('dirección').
+ *
+ * @author Philippa Bather
+ */
+
+@Repository
+public interface AddressRepository extends CrudRepository<Address, Long> {
+
+    Set<Address> getAll();
+    Set<Address> getAllByPostCode(String postcode);
+    Set<Address> getAllByRegion(Region region);
+    Set<Address> getAllByTown(String town);
+    Optional<Address> findById(long addressId);
 }
