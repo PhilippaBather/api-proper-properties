53fcf9be72a413480c1f5db879b1728e2705f072
pabather <p.a.bather1@gmail.com>
2023-12-06 19:52:44 +0100

    updated property repo

--- src/main/java/com/philippabather/properpropertiesapi/repository/PropertyRepository.java
+++ src/main/java/com/philippabather/properpropertiesapi/repository/PropertyRepository.java
@@ -1,11 +1,13 @@
 package com.philippabather.properpropertiesapi.repository;
 
 import com.philippabather.properpropertiesapi.model.Property;
+import com.philippabather.properpropertiesapi.model.PropertyStatus;
+import com.philippabather.properpropertiesapi.model.PropertyType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 /**
  * PropertyRepository - el repositorio para manejar la entidad Property ('inmueble').
@@ -15,7 +17,10 @@ import java.util.Optional;
 @Repository
 public interface PropertyRepository extends CrudRepository<Property, Long> {
 
-    List<Property> findAll();
+    Set<Property> findAll();
+    Set<Property> findAllByPropertyStatus(PropertyStatus propertyStatus);
+    Set<Property> findAllByPropertyType(PropertyType propertyType);
+    Set<Property> findAllByNumBedrooms(int numBedrooms);
     Property save(Property property);
 
     Optional<Property> findById(long propertyId);
