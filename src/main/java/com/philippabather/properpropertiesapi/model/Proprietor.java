39e7cff030c8ed63fc8b9c6dbdd7238dae903d71
pabather <p.a.bather1@gmail.com>
2023-12-10 21:09:47 +0100

    updated to manage property type lists

--- src/main/java/com/philippabather/properpropertiesapi/model/Proprietor.java
+++ src/main/java/com/philippabather/properpropertiesapi/model/Proprietor.java
@@ -1,9 +1,9 @@
 package com.philippabather.properpropertiesapi.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.philippabather.properpropertiesapi.constants.ValidationMessages;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +12,9 @@ import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.philippabather.properpropertiesapi.constants.ValidationMessages.VALIDATION_BOOLEAN_REQUIRED;
+import static com.philippabather.properpropertiesapi.constants.ValidationMessages.VALIDATION_TELEPHONE_NOT_BLANK;
 
 /**
  * Proprietor - el propietario de un inmueble.
@@ -31,7 +34,7 @@ public class Proprietor extends User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NotNull(message= ValidationMessages.VALIDATION_BOOLEAN_REQUIRED)
+    @NotNull(message= VALIDATION_BOOLEAN_REQUIRED)
     @Column(name ="is_agency")
     private boolean isAgency;
     
@@ -39,21 +42,35 @@ public class Proprietor extends User {
     @Column
     private int numProperties;
 
-    @OneToMany(mappedBy = "proprietor")
-    @JsonBackReference(value = "proprietor_properties")
-    private List<RentalProperty> propertyList = new ArrayList<>();
+    @NotBlank(message = VALIDATION_TELEPHONE_NOT_BLANK)
+    @Column
+    private String telephone;
 
-    // TODO List<SalesProperty>
+    @OneToMany(targetEntity = RentalProperty.class, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonBackReference(value = "proprietor_rental_properties")
+    private List<RentalProperty> rentalPropertyList = new ArrayList<>();
 
-    // TODO private String telephone
+    @OneToMany(targetEntity = SaleProperty.class, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonBackReference(value = "proprietor_sale_properties")
+    private List<SaleProperty> salePropertyList = new ArrayList<>();
 
     public void addRentalProperty(RentalProperty property) {
-        propertyList.add(property);
+        rentalPropertyList.add(property);
         incrementNumProperties();
     }
 
     public void removeRentalProperty(RentalProperty property) {
-        propertyList.remove(property);
+        rentalPropertyList.remove(property);
+        decrementNumProperties();
+    }
+
+    public void addSaleProperty(SaleProperty property) {
+        salePropertyList.add(property);
+        incrementNumProperties();
+    }
+
+    public void removeSaleProperty(SaleProperty property) {
+        salePropertyList.remove(property);
         decrementNumProperties();
     }
 
---
+++ src/main/java/com/philippabather/properpropertiesapi/model/SaleProperty.java
@@ -0,0 +1,2 @@
+package com.philippabather.properpropertiesapi.model;public class SaleProperty {
+}
