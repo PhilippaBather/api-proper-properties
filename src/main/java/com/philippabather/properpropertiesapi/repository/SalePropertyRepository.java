740679f674c2ac18cc05961acaabaf6adf470bb6
pabather <p.a.bather1@gmail.com>
2023-12-10 23:13:04 +0100

    updted SaleProperty repo

--- src/main/java/com/philippabather/properpropertiesapi/repository/SalePropertyRepository.java
+++ src/main/java/com/philippabather/properpropertiesapi/repository/SalePropertyRepository.java
@@ -1,2 +1,20 @@
-package com.philippabather.properpropertiesapi.repository;public interface SalePropertyRepository {
+package com.philippabather.properpropertiesapi.repository;
+
+import com.philippabather.properpropertiesapi.model.SaleProperty;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.Set;
+
+@Repository
+public interface SalePropertyRepository extends CrudRepository<SaleProperty, Long> {
+
+    Set<SaleProperty> findAll();
+    Set<SaleProperty> findAllByPrice(BigDecimal price);
+    Set<SaleProperty> findAllByConstructionDate(LocalDate constructionDate);
+    Set<SaleProperty> findAllByIsLeasehold(boolean isLeasehold);
+    Optional<SaleProperty> findById(long propertyId);
 }
