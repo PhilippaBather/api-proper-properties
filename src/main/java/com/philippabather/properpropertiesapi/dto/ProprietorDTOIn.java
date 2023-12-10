ee77085fc7505ab1ca188ea66c37c41171360c26
pabather <p.a.bather1@gmail.com>
2023-12-10 21:14:51 +0100

    updated fields; removed redundant imports

--- src/main/java/com/philippabather/properpropertiesapi/dto/ProprietorDTOIn.java
+++ src/main/java/com/philippabather/properpropertiesapi/dto/ProprietorDTOIn.java
@@ -1,16 +1,18 @@
 package com.philippabather.properpropertiesapi.dto;
 
 import com.philippabather.properpropertiesapi.constants.ValidationMessages;
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import static com.philippabather.properpropertiesapi.constants.ValidationMessages.VALIDATION_TELEPHONE_NOT_BLANK;
+
 
 /**
- * ClientDTO - Data Transfer Object (DTO) para la clase Proprietor ('propietario') que extiende la clase abstracta de User.
+ * ProprietorDTOIn - Data Transfer Object (DTO) para la clase Proprietor ('propietario') que extiende la clase abstracta de User.
  *
  * @author Philippa Bather
  */
@@ -23,5 +25,7 @@ public class ProprietorDTOIn extends UserDTOIn {
     @NotNull(message= ValidationMessages.VALIDATION_BOOLEAN_REQUIRED)
     private boolean isAgency;
 
+    @NotBlank(message = VALIDATION_TELEPHONE_NOT_BLANK)
+    private String telephone;
 
 }
