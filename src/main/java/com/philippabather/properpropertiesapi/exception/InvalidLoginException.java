60f8d5b68db7e7075b66cfda322c25fa4ec95dfe
pabather <p.a.bather1@gmail.com>
2023-12-29 10:44:31 +0100

    added invalid login exception handler

--- src/main/java/com/philippabather/properpropertiesapi/exception/InvalidLoginException.java
+++ src/main/java/com/philippabather/properpropertiesapi/exception/InvalidLoginException.java
@@ -1,2 +1,10 @@
-package com.philippabather.properpropertiesapi.exception;public class InvalidLoginException {
+package com.philippabather.properpropertiesapi.exception;
+
+import static com.philippabather.properpropertiesapi.constants.ErrorMessages.LOGIN_INVALID;
+
+public class InvalidLoginException extends RuntimeException {
+
+    public InvalidLoginException(String username) {
+        super(LOGIN_INVALID + username);
+    }
 }
