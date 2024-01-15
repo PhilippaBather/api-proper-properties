9758a94cc3fccb4f93ce506cf25e5be65ed0f072
pabather <p.a.bather1@gmail.com>
2024-01-15 22:30:16 +0100

    added logs to service files

--- src/main/java/com/philippabather/properpropertiesapi/service/ClientServiceImpl.java
+++ src/main/java/com/philippabather/properpropertiesapi/service/ClientServiceImpl.java
@@ -35,30 +35,39 @@ public class ClientServiceImpl implements ClientService {
 
     @Override
     public Set<ClientDTOOut> findAll() {
+        logger.info("start: ClientServiceImpl_findAll");
         Set<Client> clients = clientRepo.findAll();
+        logger.info("end: ClientServiceImpl_findAll");
         return convertToClientDTOOutSet(clients);
     }
 
     @Override
     public Set<ClientDTOOut> findAllBySurname(String surname) {
+        logger.info("start: ClientServiceImpl_findAllBySurname");
         Set<Client> clients = clientRepo.findAllBySurname(surname);
+        logger.info("end: ClientServiceImpl_findAllBySurname");
         return convertToClientDTOOutSet(clients);
     }
 
     @Override
     public Set<ClientDTOOut> findAllByName(String name) {
+        logger.info("start: ClientServiceImpl_findAllByName");
         Set<Client> clients = clientRepo.findAllByName(name);
+        logger.info("end: ClientServiceImpl_findAllByName");
         return convertToClientDTOOutSet(clients);
     }
 
     @Override
     public Set<ClientDTOOut> findAllByDOB(LocalDate dob) {
+        logger.info("start: ClientServiceImpl_findAllByDOB");
         Set<Client> clients = clientRepo.findAllByDob(dob);
+        logger.info("end: ClientServiceImpl_findAllByDOB");
         return convertToClientDTOOutSet(clients);
     }
 
     @Override
     public ClientDTOOut save(ClientDTOIn clientDTOIn) {
+        logger.info("start: ClientServiceImpl_save");
         // mapea la entrada a un objeto Client
         Client client = new Client();
         modelMapper.map(clientDTOIn, client);
@@ -67,19 +76,23 @@ public class ClientServiceImpl implements ClientService {
         // mapea el nuevo cliente guardado al DTO de salida
         ClientDTOOut clientDTOOut = new ClientDTOOut();
         modelMapper.map(savedClient, clientDTOOut);
+        logger.info("end: ClientServiceImpl_save");
         return clientDTOOut;
     }
 
     @Override
     public ClientDTOOut findById(long clientId) throws ClientNotFoundException {
+        logger.info("start: ClientServiceImpl_findById");
         Client client = clientRepo.findById(clientId).orElseThrow(() -> new ClientNotFoundException(clientId));
         ClientDTOOut clientDTOOut = new ClientDTOOut();
         modelMapper.map(client, clientDTOOut);
+        logger.info("end: ClientServiceImpl_findById");
         return clientDTOOut;
     }
 
     @Override
     public ClientDTOOut updateById(long clientId, ClientDTOIn clientDTOIn) throws ClientNotFoundException {
+        logger.info("start: ClientServiceImpl_updateById");
         // comprueba si el cliente existe
         Client client = clientRepo.findById(clientId).orElseThrow(() -> new ClientNotFoundException(clientId));
 
@@ -92,16 +105,20 @@ public class ClientServiceImpl implements ClientService {
         ClientDTOOut clientDTOOut = new ClientDTOOut();
         modelMapper.map(updatedClient, clientDTOOut);
 
+        logger.info("end: ClientServiceImpl_updateById");
         return clientDTOOut;
     }
 
     @Override
     public void deleteById(long clientId) throws ClientNotFoundException {
+        logger.info("start: ClientServiceImpl_deleteById");
         Client client = clientRepo.findById(clientId).orElseThrow(() -> new ClientNotFoundException(clientId));
         clientRepo.delete(client);
+        logger.info("end: ClientServiceImpl_deleteById");
     }
 
     private Set<ClientDTOOut> convertToClientDTOOutSet(Set<Client> clients) {
+        logger.info("start: ClientServiceImpl_convertToClientDTOOutSet");
         Set<ClientDTOOut> clientsDTOOut = new HashSet<>();
 
         for (Client client :
@@ -111,6 +128,7 @@ public class ClientServiceImpl implements ClientService {
             modelMapper.map(client, clientDTOOut);
             clientsDTOOut.add(clientDTOOut);
         }
+        logger.info("end: ClientServiceImpl_convertToClientDTOOutSet");
         return clientsDTOOut;
     }
 }
--- src/main/java/com/philippabather/properpropertiesapi/service/RentalPropertyServiceImpl.java
+++ src/main/java/com/philippabather/properpropertiesapi/service/RentalPropertyServiceImpl.java
@@ -9,6 +9,8 @@ import com.philippabather.properpropertiesapi.model.RentalProperty;
 import com.philippabather.properpropertiesapi.repository.ProprietorRepository;
 import com.philippabather.properpropertiesapi.repository.RentalPropertyRepository;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -24,6 +26,7 @@ import java.util.Set;
 @Service
 public class RentalPropertyServiceImpl implements RentalPropertyService {
 
+    private final Logger logger = LoggerFactory.getLogger(RentalPropertyServiceImpl.class);
     private final ProprietorService proprietorService;
     private final ProprietorRepository proprietorRepo;
     private final RentalPropertyRepository rentalRepo;
@@ -39,30 +42,39 @@ public class RentalPropertyServiceImpl implements RentalPropertyService {
 
     @Override
     public Set<RentalDTOOut> findAll() {
+        logger.info("start: RentalPropertyServiceImpl_findAll");
         Set<RentalProperty> rentals = rentalRepo.findAll();
+        logger.info("end: RentalPropertyServiceImpl_findAll");
         return convertToRentalDTOOutSet(rentals);
     }
 
     @Override
     public Set<RentalDTOOut> findAllByMonthlyRent(BigDecimal rentPerMonth) {
+        logger.info("start: RentalPropertyServiceImpl_findAllByMonthlyRent");
         Set<RentalProperty> rentals = rentalRepo.findAllByRentPerMonth(rentPerMonth);
+        logger.info("end: RentalPropertyServiceImpl_findAllByMonthlyRent");
         return convertToRentalDTOOutSet(rentals);
     }
 
     @Override
     public Set<RentalDTOOut> findAllByMinTenancy(int minTenancy) {
+        logger.info("start: RentalPropertyServiceImpl_findAllByMinTenancy");
         Set<RentalProperty> rentals = rentalRepo.findAllByMinTenancy(minTenancy);
+        logger.info("end: RentalPropertyServiceImpl_findAllByMinTenancy");
         return convertToRentalDTOOutSet(rentals);
     }
 
     @Override
     public Set<RentalDTOOut> findByNumBedrooms(int numBedrooms) {
+        logger.info("start: RentalPropertyServiceImpl_findByNumBedrooms");
         Set<RentalProperty> rentals = rentalRepo.findAllByNumBedrooms(numBedrooms);
+        logger.info("end: RentalPropertyServiceImpl_findByNumBedrooms");
         return convertToRentalDTOOutSet(rentals);
     }
 
     @Override
     public RentalDTOOut save(long proprietorId, RentalProperty rentalProperty) throws ProprietorNotFoundException {
+        logger.info("start: RentalPropertyServiceImpl_save");
         // coge el objeto de Proprietor o lanza una excepción
         Proprietor proprietor = proprietorRepo.findById(proprietorId).orElseThrow(() -> new ProprietorNotFoundException(proprietorId));
 
@@ -77,17 +89,21 @@ public class RentalPropertyServiceImpl implements RentalPropertyService {
         proprietor.addRentalProperty(savedProperty);
         proprietorService.updatePropertyDetails(proprietor);
 
+        logger.info("end: RentalPropertyServiceImpl_save");
         return mapToRentalDTOOut(savedProperty);
     }
 
     @Override
     public RentalDTOOut findById(long propertyId) throws PropertyNotFoundException {
+        logger.info("start: RentalPropertyServiceImpl_findById");
         RentalProperty rental = rentalRepo.findById(propertyId).orElseThrow(() -> new PropertyNotFoundException(propertyId));
+        logger.info("end: RentalPropertyServiceImpl_findById");
         return mapToRentalDTOOut(rental);
     }
 
     @Override
     public RentalDTOOut updateById(long propertyId, RentalProperty rentalPropertyDTOIn) throws PropertyNotFoundException {
+        logger.info("start: RentalPropertyServiceImpl_updateById");
         RentalProperty property = rentalRepo.findById(propertyId).orElseThrow(() -> new PropertyNotFoundException(propertyId));
 
         // coge el objeto Proprietor
@@ -107,11 +123,13 @@ public class RentalPropertyServiceImpl implements RentalPropertyService {
         proprietor.addRentalProperty(property);
         proprietorService.updatePropertyDetails(proprietor);
 
+        logger.info("end: RentalPropertyServiceImpl_updateById");
         return mapToRentalDTOOut(updatedProperty);
     }
 
     @Override
     public void deleteById(long propertyId) throws PropertyNotFoundException {
+        logger.info("start: RentalPropertyServiceImpl_deleteById");
         RentalProperty property = rentalRepo.findById(propertyId).orElseThrow(() -> new PropertyNotFoundException(propertyId));
 
         // actualiza el objeto Proprietor
@@ -120,16 +138,20 @@ public class RentalPropertyServiceImpl implements RentalPropertyService {
         proprietorService.updatePropertyDetails(proprietor);
 
         rentalRepo.delete(property);
+        logger.info("end: RentalPropertyServiceImpl_deleteById");
     }
 
     @Override
     public void deleteAddressById(long propertyId) throws PropertyNotFoundException {
+        logger.info("start: RentalPropertyServiceImpl_deleteAddressById");
         RentalProperty property = rentalRepo.findById(propertyId).orElseThrow(() -> new PropertyNotFoundException(propertyId));
         property.setAddress(null);
         rentalRepo.save(property);
+        logger.info("end: RentalPropertyServiceImpl_deleteAddressById");
     }
 
     private Set<RentalDTOOut> convertToRentalDTOOutSet(Set<RentalProperty> rentalProperties) {
+        logger.info("start: RentalPropertyServiceImpl_convertToRentalDTOOutSet");
         Set<RentalDTOOut> rentalsDTOOut = new HashSet<>();
 
         for (RentalProperty rental :
@@ -139,12 +161,15 @@ public class RentalPropertyServiceImpl implements RentalPropertyService {
             rentalsDTOOut.add(rentalDTOOut);
         }
 
+        logger.info("end: RentalPropertyServiceImpl_convertToRentalDTOOutSet");
         return rentalsDTOOut;
     }
 
     private RentalDTOOut mapToRentalDTOOut(RentalProperty rentalProperty) {
+        logger.info("start: RentalPropertyServiceImpl_mapToRentalDTOOut");
         RentalDTOOut rentalDTOOut = new RentalDTOOut();
         modelMapper.map(rentalProperty, rentalDTOOut);
+        logger.info("end: RentalPropertyServiceImpl_mapToRentalDTOOut");
         return rentalDTOOut;
     }
 }
