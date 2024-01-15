ab0b49765a70e86b88a2855685f111e89b49c758
pabather <p.a.bather1@gmail.com>
2024-01-15 21:51:48 +0100

    added controller logs

--- src/main/java/com/philippabather/properpropertiesapi/config/AppConfig.java
+++ src/main/java/com/philippabather/properpropertiesapi/config/AppConfig.java
@@ -6,10 +6,13 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import com.philippabather.properpropertiesapi.controller.ProprietorController;
 import com.philippabather.properpropertiesapi.security.AuthEntryPointJwt;
 import com.philippabather.properpropertiesapi.security.AuthTokenFilter;
 import com.philippabather.properpropertiesapi.security.ProprietorUserDetailsService;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -41,6 +44,8 @@ import java.security.interfaces.RSAPublicKey;
 @Configuration
 public class AppConfig{
 
+    private static final Logger logger = LoggerFactory.getLogger(AppConfig.class);
+
     @Bean
     public ModelMapper ModelMapper() {
         return new ModelMapper();
@@ -63,31 +68,37 @@ public class AppConfig{
 
     @Bean
     public AuthTokenFilter authenticationJwtTokenFilter() {
+        logger.info("AppConfig_authenticationJwtTokenFilter");
         return new AuthTokenFilter();
     }
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
+        logger.info("start: AppConfig_authenticationProvider");
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 
         authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
 
+        logger.info("end: AppConfig_authenticationProvider");
         return authProvider;
     }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        logger.info("AppConfig_authenticationManager");
         return authConfig.getAuthenticationManager();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
+        logger.info("AppConfig_passwordEncoder");
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        logger.info("start: AppConfig_filterChain");
         http.csrf(csrf -> csrf.disable())
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -107,11 +118,13 @@ public class AppConfig{
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
+        logger.info("end: AppConfig_filterChain");
         return http.build();
     }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
+        logger.info("AppConfig_webSecurityCustomizer");
         return (web) -> web.ignoring().requestMatchers(
                 "/users/clients/**", "/users/proprietors/**",
                 "/properties/rental", "properties/sale",
@@ -120,13 +133,16 @@ public class AppConfig{
 
     @Bean
     JwtDecoder jwtDecoder() {
+        logger.info("AppConfig_jwtDecoder");
         return NimbusJwtDecoder.withPublicKey(this.key).build();
     }
 
     @Bean
     JwtEncoder jwtEncoder() {
+        logger.info("start: AppConfig_jwtEncoder");
         JWK jwk = new RSAKey.Builder(this.key).privateKey(this.priv).build();
         JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        logger.info("end: AppConfig_jwtEncoder");
         return new NimbusJwtEncoder(jwks);
     }
 
