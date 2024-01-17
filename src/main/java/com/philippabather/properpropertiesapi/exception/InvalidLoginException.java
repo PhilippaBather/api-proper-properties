addc6787d499d8de45682356e7ae9b558b6d90d8
pabather <p.a.bather1@gmail.com>
2024-01-17 01:48:45 +0100

    updated comments; removed unused imports

--- src/main/java/com/philippabather/properpropertiesapi/exception/InvalidLoginException.java
+++ src/main/java/com/philippabather/properpropertiesapi/exception/InvalidLoginException.java
@@ -2,6 +2,11 @@ package com.philippabather.properpropertiesapi.exception;
 
 import static com.philippabather.properpropertiesapi.constants.ErrorMessages.LOGIN_INVALID;
 
+/**
+ * InvalidLoginException - maneja el error lanzado cuando las detalles de login son incorrectos.
+ *
+ * @author Philippa Bather
+ */
 public class InvalidLoginException extends RuntimeException {
 
     public InvalidLoginException(String username) {
