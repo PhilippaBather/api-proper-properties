bd6aa1d6bd18455fca4f61a2dbd0274db3e7b7d3
pabather <p.a.bather1@gmail.com>
2024-01-17 01:46:51 +0100

    updated comments; removed unused imports

--- src/main/java/com/philippabather/properpropertiesapi/dto/LoginDTOIn.java
+++ src/main/java/com/philippabather/properpropertiesapi/dto/LoginDTOIn.java
@@ -2,6 +2,11 @@ package com.philippabather.properpropertiesapi.dto;
 
 import lombok.Data;
 
+/**
+ * LoginDTOIn - El objeto de la transferencia de datos (DTO) para las detalles de login
+ *
+ * @author Philippa Bather
+ */
 @Data
 public class LoginDTOIn {
     private String username;
